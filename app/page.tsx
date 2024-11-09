5f1e25a94ef7be8cfb43e52ba1a721d36f9db4eb
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-09 22:33:38 +0500

    homep

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import Footer from '../components/Footer'
+import Homep from '../components/home'
 export default function Home() {
   return (
     <>
     <Header />
+    <Homep />
     <Footer />
     </>
   );
