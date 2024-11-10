1777842511b1e2f73c5491fa937349725fe4be55
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 09:07:56 +0500

    newpush

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Cont from "../components/Mainp"
 export default function Home() {
   return (
     <>
     <Header />
+    <Cont />
     <Footer />
     </>
   );
