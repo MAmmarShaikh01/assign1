1434c1e803ccd6e928d103f43a77a4f6018ed9c8
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 09:33:31 +0500

    n

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,10 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import Contact from "../../components/contact";
 
 export default function Home() {
   return (
     <>
     <Header />
-    <Contact />
     <Footer />
     </>
   );
