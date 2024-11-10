327a851e854f86228467d7df9c18c4d02651bb41
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 09:19:22 +0500

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-// import Cont from "../components/Mainp"
+import Cont from "../../components/about"
 export default function Home() {
   return (
     <>
     <Header />
-    {/* <Cont /> */}
+    <Cont />
     <Footer />
     </>
   );
