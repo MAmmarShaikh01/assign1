d8babcee7365480444a7d8c838806e6dc03632fb
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 09:01:38 +0500

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import Main from "../components/index";
 export default function Home() {
   return (
     <>
     <Header />
-    <Main />
     <Footer />
     </>
   );
