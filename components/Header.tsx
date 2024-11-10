1d5634b8af5093360feb2e668920ff3457c0293f
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 06:36:15 +0500

    header

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+// components/Header.tsx
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.logo}>
+          <Link href="/">My Portfolio</Link>
+        </h1>
+        <nav className={styles.nav}>
+          <Link href="/" className={styles.navLink}>Home</Link>
+          <Link href="/about" className={styles.navLink}>About</Link>
+          <Link href="/contact" className={styles.navLink}>Contact</Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
