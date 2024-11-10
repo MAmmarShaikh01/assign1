cbacd875254cfce448218d46d1d2fdde1e106780
MAmmarShaikh01 <shaikhurwa61@gmail.com>
2024-11-10 06:47:49 +0500

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 export default function Home() {
   return (
     <>
     <Header />
   <h1>h1</h1>
+    <Footer />
     </>
   );
 }
